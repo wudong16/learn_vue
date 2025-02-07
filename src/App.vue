a38099ba9f9146d08fb0a70d44ba629f0b3ddc55
wudong <3045875724@qq.com>
2025-02-07 18:08:18 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <Person/>
+</template>
+
+<script lang="ts">
+    import Person from './components/Person.vue';
+    export default{
+        name : 'APP',  //组件名
+        components: {Person}   // 注册组件
+    }
+</script>
---
+++ 备份/01_自己写App组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- html -->
+     <div class="app">
+        <h1>你好啊!</h1>
+     </div>
+</template>
+
+<script lang="ts">
+// JS 或 TS
+export default{
+    name : 'App'  // 组件名
+}
+</script>
+
+<style>
+/* 样式 */
+.app{
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
