a18ffad0c03dfde3f6514063d40ba124097b1747
wudong <3045875724@qq.com>
2025-02-07 22:10:01 +0800

    保存02_setup语法糖

---
+++ 备份/02_setup语法糖/components/Person.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{name}}</h2>
+        <h2>年龄：{{age}}</h2>
+        <h2>地址：{{address}}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts" setup name = "Person">
+    // 数据，原来是写在data中的，此时的name、age、tel都不是响应式的数据
+    let name = '张三'
+    let age = 18
+    let tel = '13888888888'
+    let address = '重庆市沙坪坝区'
+
+    // 方法
+    function changeName(){
+        name = 'zhangsan'
+        console.log(name)
+    }
+    function changeAge(){
+        age += 1
+        console.log(age)
+    }
+    function showTel(){
+        alert(tel)
+    }
+</script>
+
+<style scoped>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        margin: 0 5px;
+    }
+</style>
